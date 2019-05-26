2010d47054b6d8ddfbb4a9eac9539022e7270119
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2019-05-27 01:37:26 +0200

    Draw random cells

--- src/maze/core.cljs
+++ src/maze/core.cljs
@@ -7,7 +7,12 @@
 (defn init-canvas! []
   (.getContext (.getElementById js/document "canvas") "2d"))
 
+(defn move-to [ctx [x y]] (.moveTo ctx x y))
+(defn line-to [ctx [x y]] (.lineTo ctx x y))
+
 (def ctx (init-canvas!))
+
+(def CELLS [#{:east :south} #{:east} #{:south} #{}])
 
 (defn corners [x y]
   [[(* x CELL_SIZE) (* y CELL_SIZE)]
@@ -15,25 +20,28 @@
    [(* (inc x) CELL_SIZE) (* (inc y) CELL_SIZE)]
    [(* x CELL_SIZE) (* (inc y) CELL_SIZE)]])
 
-(defn move-to [ctx [x y]] (.moveTo ctx x y))
-(defn line-to [ctx [x y]] (.lineTo ctx x y))
+(defn draw-cell [ctx x y walls]
+  (.beginPath ctx)
+  (doseq [wall walls]
+    (case wall
+      :east (do (move-to ctx [(* (inc x) CELL_SIZE) (* y CELL_SIZE)])
+                (line-to ctx [(* (inc x) CELL_SIZE) (* (inc y) CELL_SIZE)]))
+      :south (do (move-to ctx [(* x CELL_SIZE) (* (inc y) CELL_SIZE)])
+                 (line-to ctx [(* (inc x) CELL_SIZE) (* (inc y) CELL_SIZE)]))))
+  (.stroke ctx))
 
 (defn draw! [ctx cells]
   (set! (.-fillStyle ctx) "rgb(255,255,255)")
   (.fillRect ctx 0 0 SIZE SIZE)
 
-  (set! (.-lineWidth ctx) 5)
+  (set! (.-lineWidth ctx) 3)
   (set! (.-strokeStyle ctx) "#223")
   (.strokeRect ctx 0 0 SIZE SIZE)
 
   (doseq [x (range GRID)]
     (doseq [y (range GRID)]
-      (let [[a b c d] (corners x y)]
-        (.beginPath ctx)
-        (move-to ctx d)
-        (line-to ctx c)
-        (line-to ctx b)
-        (.stroke ctx)))))
+      (draw-cell ctx x y (rand-nth CELLS)))))
 
 (defn main! []
-  (.log js/console "MAIN"))
+  (.log js/console "MAIN")
+  (draw! ctx []))
