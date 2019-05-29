4087f5d354b4aa643358777ae1b77a1d74b7a1fc
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2019-05-29 07:17:54 +0200

    Maze as a connected graph and randomize

--- src/maze/core.cljs
+++ src/maze/core.cljs
@@ -1,46 +1,73 @@
-(ns maze.core)
+(ns maze.core
+  (:require [clojure.set :as set]))
+
+(defn- log [x]
+  (.log js/console (clj->js x)))
 
 (def SIZE 500)
 (def GRID 20)
+(def WALL_SIZE 2)
 (def CELL_SIZE (/ SIZE GRID))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Canvas / Drawing
 
 (defn init-canvas! []
   (.getContext (.getElementById js/document "canvas") "2d"))
 
+(def ctx (init-canvas!))
+
 (defn move-to [ctx [x y]] (.moveTo ctx x y))
 (defn line-to [ctx [x y]] (.lineTo ctx x y))
 
-(def ctx (init-canvas!))
+(defn draw-line [ctx line]
+  (move-to ctx (first line))
+  (line-to ctx (second line)))
 
-(def CELLS [#{:east :south} #{:east} #{:south} #{}])
 (set! (.-fillStyle ctx) "rgb(255,255,255)")
-(set! (.-lineWidth ctx) 2)
+(set! (.-lineWidth ctx) WALL_SIZE)
 (set! (.-strokeStyle ctx) "#223")
 
-(defn corners [x y]
-  [[(* x CELL_SIZE) (* y CELL_SIZE)]
-   [(* (inc x) CELL_SIZE) (* y CELL_SIZE)]
+(defn corners [[x y]]
+  [[(* x       CELL_SIZE) (* y       CELL_SIZE)]
+   [(* (inc x) CELL_SIZE) (* y       CELL_SIZE)]
    [(* (inc x) CELL_SIZE) (* (inc y) CELL_SIZE)]
-   [(* x CELL_SIZE) (* (inc y) CELL_SIZE)]])
+   [(* x       CELL_SIZE) (* (inc y) CELL_SIZE)]])
 
-(defn draw-cell [ctx x y walls]
+(defn- draw-wall [ctx c1 c2]
   (.beginPath ctx)
-  (doseq [wall walls]
-    (case wall
-      :east (do (move-to ctx [(* (inc x) CELL_SIZE) (* y CELL_SIZE)])
-                (line-to ctx [(* (inc x) CELL_SIZE) (* (inc y) CELL_SIZE)]))
-      :south (do (move-to ctx [(* x CELL_SIZE) (* (inc y) CELL_SIZE)])
-                 (line-to ctx [(* (inc x) CELL_SIZE) (* (inc y) CELL_SIZE)]))))
+  (let [line (set/intersection (set (corners c1)) (set (corners c2)))]
+    (assert (= 2 (count line)) line)
+    (draw-line ctx (vec line)))
   (.stroke ctx))
 
 (defn draw! [ctx maze]
   (.fillRect ctx 0 0 SIZE SIZE)
   (.strokeRect ctx 0 0 SIZE SIZE)
+  (doseq [[[c1 c2] wall?] maze]
+    (when wall? (draw-wall ctx c1 c2))))
 
-  (doseq [[x row] (map-indexed vector maze)]
-    (doseq [[y cell] ((map-indexed vector row))]
-      (draw-cell ctx x y cell))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Maze
+
+(defn- new-maze
+  "A maze is a graph between cells, where the edges are a wall (boolean)."
+  []
+  {})
+
+(defn- assoc-wall [maze c1 c2]
+  (assoc maze [c1 c2] true))
+
+(defn- all-cells []
+  (mapcat #(map vector (repeat %) (range GRID)) (range GRID)))
+
+(defn- random-maze []
+  (reduce
+   (fn [maze [cell offset]]
+     (assoc-wall maze cell [(+ (first cell) (first offset))
+                            (+ (second cell) (second offset))]))
+   (new-maze)
+   (for [cell (all-cells)] [cell (rand-nth [[0 1] [1 0]])])))
 
 (defn main! []
-  (.log js/console "MAIN")
-  (draw! ctx []))
+  (draw! ctx (random-maze)))
