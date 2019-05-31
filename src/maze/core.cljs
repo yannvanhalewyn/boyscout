ab819dde0c169a8f890f69eabaf75a93223bc113
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2019-05-31 13:54:54 +0200

    Depth first search through any graph with async filling cells

--- src/maze/core.cljs
+++ src/maze/core.cljs
@@ -1,11 +1,13 @@
 (ns maze.core
-  (:require [clojure.set :as set]))
+  (:require [clojure.set :as set]
+            [clojure.core.async :as a
+             :refer-macros [go go-loop]]))
 
 (defn- log [x]
   (.log js/console (clj->js x)))
 
 (def SIZE 600)
-(def GRID 20)
+(def GRID 10)
 (def WALL_SIZE 2)
 (def CELL_SIZE (/ SIZE GRID))
 
@@ -37,13 +39,15 @@
     (draw-line ctx (vec line)))
   (.stroke ctx))
 
+(defn- fill-cell [ctx color [x y :as cell]]
+  (set! (.-fillStyle ctx) color)
+  (.fillRect ctx (* x CELL_SIZE) (* y CELL_SIZE) CELL_SIZE CELL_SIZE))
+
 (defn draw! [ctx maze]
   (set! (.-fillStyle ctx) "rgb(255,255,255)")
   (.fillRect ctx 0 0 SIZE SIZE)
-  (set! (.-fillStyle ctx) "#5d5")
-  (.fillRect ctx 0 0 CELL_SIZE CELL_SIZE)
-  (set! (.-fillStyle ctx) "#d55")
-  (.fillRect ctx (- SIZE CELL_SIZE) (- SIZE CELL_SIZE) CELL_SIZE CELL_SIZE)
+  (fill-cell ctx "#5d5" [0 0])
+  (fill-cell ctx "#d55" [(dec GRID) (dec GRID)])
 
   (set! (.-lineWidth ctx) WALL_SIZE)
   (set! (.-strokeStyle ctx) "#223")
@@ -62,6 +66,10 @@
   (if (in-maze? c2)
     (assoc-in maze [c1 c2] false)
     maze))
+
+(defn- neighbors [maze cell]
+  (concat (keys (get maze cell))
+          (filter #(contains? (get maze %) cell) (keys maze))))
 
 (defn- empty-maze
   "A maze is a graph between cells, where the edges are a wall (boolean).
@@ -88,6 +96,42 @@
         #(assoc-in %1 [cell %2] (rand-nth [true false]))
         maze walls))
      maze maze)))
+
+(defn- depth-first [graph start {:keys [visit-fn neighbors-fn]}]
+  (loop [stack (vector start)
+         visited? #{}]
+    (if (empty? stack)
+      (vec visited?)
+      (let [node (peek stack)
+            neighbors (neighbors-fn graph node)
+            new-stack (into [] (remove visited?) (into (pop stack) neighbors))]
+        (visit-fn node)
+        (if (visited? node)
+          (recur new-stack visited?)
+          (recur new-stack (conj visited? node)))))))
+
+(comment
+
+  (def DRAW_TIMEOUT 100)
+
+  (let [c (a/chan)]
+    (draw! ctx (random-maze))
+    (go-loop []
+      (if-let [cell (a/<! c)]
+        (do (fill-cell ctx "blue" cell)
+            (a/<! (a/timeout DRAW_TIMEOUT))
+            (recur))
+        (.log js/console "CLOSE")))
+
+    (depth-first (empty-maze) [0 0]
+                 {:visit-fn
+                  (fn [cell]
+                    (a/put! c cell))
+                  :neighbors-fn (comp (partial sort-by #(rand-int 100)) neighbors)})
+
+    (a/close! c))
+
+  )
 
 (defn main! []
   (draw! ctx (random-maze)))
