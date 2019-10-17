47d545a5f53e0c0d03b0713996f548bc4e946ac5
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2019-10-17 08:20:43 +0200

    Check in built files

---
+++ build/img/generate.clj
@@ -0,0 +1,82 @@
+(ns public.img.generate
+  (:require [dorothy.core :as dot]
+            [dorothy.jvm :refer (save!)]))
+
+(defn create-svg [filename data]
+  (-> data
+      (conj (dot/node-attrs {:fontname "sans-serif"}))
+      (conj (dot/edge-attrs {:fontname "sans-serif"}))
+      (conj (dot/graph-attrs {:overlap "false"
+                              :splines "true"
+                              :fontname "sans-serif"
+                              :sep "+20"}))
+      (dot/digraph)
+      (dot/dot)
+      (save! filename {:format :svg :layout :fdp})))
+
+(defn- make-node [k & [type]]
+  (let [[font text] (get {:target [2 3]
+                          :path [2 3]
+                          :visit [6 7]
+                          nil ["black" "lightgray"]} type)]
+    [k {:shape :Mcircle
+        :style :filled
+        :fontsize 10
+        :colorscheme "paired12"
+        :fontcolor (if type (mod font 12) font)
+        :color (if type (mod text 12) text)
+        :label (name k)}]))
+
+(def DIJKSTRA
+  (list (make-node "Start" :target)
+        (make-node :A :visit)
+        (make-node :B :path)
+        (make-node :C :visit)
+        (make-node :D :visit)
+        (make-node :E)
+        (make-node :F :visit)
+        (make-node "End" :target)
+        (make-node :H :path)
+        (make-node :I)
+        (make-node :J)
+        (make-node :K)
+        (make-node :L)
+        ["Start" :A :D :F :H {:dir :none}]
+        ["Start" :B :H "End" {}]
+        [:A :B {:dir :none}]
+        ["Start" :C :L :I :K :E "End" {:dir :none}]
+        [:K :J :L {:dir :none}]))
+
+(def DFS
+  (list (make-node "Start" :target)
+        (make-node :A :visit)
+        (make-node :D :visit)
+        (make-node :F :visit)
+        (make-node :B :path)
+        (make-node :C)
+        (make-node :E)
+        (make-node "End" :target)
+        ["Start" :A :D :F {:dir :none}]
+        ["Start" :B "End" {}]
+        [:A :B {:dir :none}]
+        ["Start" :C :E "End" {:dir :none}]))
+
+(def BFS
+  (list (make-node "Start" :target)
+        (make-node :A :visit)
+        (make-node :D :visit)
+        (make-node :F)
+        (make-node :B :path)
+        (make-node :C :visit)
+        (make-node :E)
+        (make-node "End" :target)
+        ["Start" :A :D :F {:dir :none}]
+        ["Start" :B "End" {}]
+        [:A :B {:dir :none}]
+        ["Start" :C :E "End" {:dir :none}]))
+
+(doseq [[file data] [["resources/public/img/breadth-first.svg" BFS]
+                     ["resources/public/img/depth-first.svg" DFS]
+                     ["resources/public/img/dijkstra.svg" DIJKSTRA]]]
+  (create-svg file data)
+  (clojure.java.shell/sh "open" "-a" "Google Chrome" file))
