85a1e63198a74815b4ea72c35a6440ee281800c1
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2019-10-18 23:59:43 +0200

    Add link to github

--- src/bs/core.cljs
+++ src/bs/core.cljs
@@ -1,22 +1,29 @@
 (ns bs.core
   (:require [bs.db :as db]
             [bs.ui :as ui]
+            [bs.utils :as u]
             [reagent.core :as r]))
 
 (defonce db (r/atom (db/new-db)))
 
 (defn root [db]
   [:<>
+   [:div.px-24.py-2.bg-gray-100.shadow
+    [:a.pt2.btn.float-right.align-middle.text-gray-800.hover:bg-gray-200
+     {:style {:line-height "34px"}
+      :href "https://www.github.com/yannvanhalewyn/boyscout"
+      :target "_blank"}
+     [:span "View source on"]
+     [:img.inline.ml-4 {:src (u/resource-path "img/github-mark-32.png")}]]
+    [:span.text-blue-700.align-middle
+     [:i.mdi.mdi-tent.text-4xl]
+     [:h1.ml-3.inline-block.font-logo.font-bold.text-4xl.tracking-widest "Boyscout"]]
+    [:span.ml-10.text-gray-600.align-middle "A pathfinding and maze generation visualizer"]]
    (let [error (:db/error @db)]
      [:div.alert.w-full.px-4.py-6.text-center {:class (when error "alert--is-open")}
       [:button.mdi.mdi-close.text-lg.float-right.hover:text-red-900
        {:on-click #(db/hide-error! db)}]
       [:p error]])
-   [:div.px-24.py-2.bg-gray-100.shadow-md
-    [:span.text-blue-700.align-middle
-     [:i.mdi.mdi-tent.text-4xl]
-     [:h1.ml-3.inline-block.font-logo.font-bold.text-4xl.tracking-widest "Boyscout"]]
-    [:span.ml-10.text-gray-600.align-middle "A pathfinding maze generating visualizer"]]
    [:div.flex.mt-8.flex-wrap.max-w-7xl.m-auto
     [:div.px-8 {:class "xl:w-1/3"}
      [ui/sidebar db]]
