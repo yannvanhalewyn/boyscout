d47a85d25f1ddef5a25e3996474f52d5f61bd278
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2019-10-18 23:41:15 +0200

    Restyle the header

--- src/bs/core.cljs
+++ src/bs/core.cljs
@@ -12,11 +12,11 @@
       [:button.mdi.mdi-close.text-lg.float-right.hover:text-red-900
        {:on-click #(db/hide-error! db)}]
       [:p error]])
-   [:div.px-16.py-3.bg-blue-600.text-center.text-white.text-3xl.font-bold.shadow-md
-    [:i.mdi.mdi-tent]
-    [:h1.ml-3.inline-block "Boyscout"]]
-   [:div.flex.mt-8.flex-wrap
-    [:div.px-8 {:class "xl:w-1/4"}
+   [:div.px-24.py-2.bg-gray-100.shadow-md
+    [:span.text-blue-700.align-middle
+     [:i.mdi.mdi-tent.text-4xl]
+     [:h1.ml-3.inline-block.font-logo.font-bold.text-4xl.tracking-widest "Boyscout"]]
+    [:span.ml-10.text-gray-600.align-middle "A pathfinding maze generating visualizer"]]
    [:div.flex.mt-8.flex-wrap.max-w-7xl.m-auto
     [:div.px-8 {:class "xl:w-1/3"}
      [ui/sidebar db]]
